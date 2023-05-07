15dde9907cda53a0a8c18bc0639e26b3eb704b75
Lingstreasure <1486520814@qq.com>
2023-05-07 07:41:59 +0000

    add source text for polyhaven and sharetextures

--- data_process/code/polyhaven_text_generate.py
+++ data_process/code/polyhaven_text_generate.py
@@ -27,37 +27,42 @@ for i in range(0, len(names)):
     os.makedirs(text_dir, exist_ok=True)
     
     ## source text
-    source_keys = dir_name.split('-')[:-1]
-    source_text = ' '.join(source_keys)
+    source_keys = dir_name.split('_')
+    filtered_keys = []
+    for j, k in enumerate(source_keys):
+        if k[0].isalpha():
+            filtered_keys.append(source_keys[j])
+            
+    source_text = ' '.join(filtered_keys)
     source_text_path = opt.join(text_dir, 'source_text.txt')
     with open(source_text_path, 'w') as f:
         f.write(source_text)
         
     ## text without pattern
-    text_wo_pattern = str()
-    with open(text_wo_pattern_path, 'r') as f:
-        text_wo_pattern = f.read().strip()
-    text_wo_pattern_new_path = opt.join(text_dir, 'text_wo_pattern.txt')
-    os.system(f"cp {text_wo_pattern_path} {text_wo_pattern_new_path}")
+    # text_wo_pattern = str()
+    # with open(text_wo_pattern_path, 'r') as f:
+    #     text_wo_pattern = f.read().strip()
+    # text_wo_pattern_new_path = opt.join(text_dir, 'text_wo_pattern.txt')
+    # os.system(f"cp {text_wo_pattern_path} {text_wo_pattern_new_path}")
     
     ## full text
-    full_text = str()
-    with open(full_text_path, 'r') as f:
-        full_text = f.read().strip()
-    full_text_new_path = opt.join(text_dir, 'full_text.txt')
-    os.system(f"cp {full_text_path} {full_text_new_path}")
+    # full_text = str()
+    # with open(full_text_path, 'r') as f:
+    #     full_text = f.read().strip()
+    # full_text_new_path = opt.join(text_dir, 'full_text.txt')
+    # os.system(f"cp {full_text_path} {full_text_new_path}")
     
     ## text without color
-    text_wo_color_keys = []
-    keys = full_text.split(' ')
-    for k in keys:
-        if k not in colors:
-            text_wo_color_keys.append(k)          
+    # text_wo_color_keys = []
+    # keys = full_text.split(' ')
+    # for k in keys:
+    #     if k not in colors:
+    #         text_wo_color_keys.append(k)          
 
-    text_wo_color = ' '.join(text_wo_color_keys).strip()
-    text_wo_color_path = opt.join(text_dir, 'text_wo_color.txt')
-    with open(text_wo_color_path, 'w') as f:
-        f.write(text_wo_color)
+    # text_wo_color = ' '.join(text_wo_color_keys).strip()
+    # text_wo_color_path = opt.join(text_dir, 'text_wo_color.txt')
+    # with open(text_wo_color_path, 'w') as f:
+    #     f.write(text_wo_color)
     
     print(f"{i} {dir_name}")
     # assert 0
